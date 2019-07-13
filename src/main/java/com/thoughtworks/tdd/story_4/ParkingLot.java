d18abb1d8287afed75eda0fab778d8d395018351
Tiger Zhu <tiger.zhu@oocl.com>
2019-07-13 18:28:29 +0800

    finish test story4

---
+++ src/main/java/com/thoughtworks/tdd/story_4/ParkingLot.java
@@ -0,0 +1,35 @@
+package com.thoughtworks.tdd.story_4;
+
+
+
+
+import java.util.List;
+
+public class ParkingLot {
+    private List<String> carNumList;
+    private int capacity;
+    public List<String> getCarNumList() {
+        return carNumList;
+    }
+
+    public ParkingLot(List<String> carNumList) {
+        this.carNumList = carNumList;
+        this.capacity=10;
+    }
+
+    public void setCarNumList(List<String> carNumList) {
+        this.carNumList = carNumList;
+    }
+    public Ticket addTheCarToPakingLot(String carNum){
+        carNumList.add(carNum);
+        return new Ticket(carNum);
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
---
+++ src/main/java/com/thoughtworks/tdd/story_4/Ticket.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.tdd.story_4;
+
+public class Ticket {
+    private String ticketNum;
+    private Boolean isVaild;
+
+    public Ticket(String carNum) {
+        this.ticketNum = carNum;
+        this.setVaild(true);
+    }
+
+    public Boolean getVaild() {
+        return isVaild;
+    }
+
+    public void setVaild(Boolean vaild) {
+        isVaild = vaild;
+    }
+
+    public String getTicketNum() {
+        return ticketNum;
+    }
+
+    public void setTicketNum(String ticketNum) {
+        this.ticketNum = ticketNum;
+    }
+
+
+}
